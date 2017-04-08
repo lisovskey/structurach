6e73ced75083515895e6d88c2d2b297ede8c49f9
lisovskey <ritofzeed@gmail.com>
2017-04-08 21:56:43 +0300

    useless braces

--- include/tree.h
+++ include/tree.h
@@ -42,8 +42,8 @@ class Tree
     pointer _insert(pointer leaf, pair data)
     {
         if      (!leaf)                             return leaf = pointer(new node(data));
-        else if (data.first < leaf->data.first)     leaf->left = (pointer)_insert(leaf->left, data);
-        else if (data.first > leaf->data.first)     leaf->right = (pointer)_insert(leaf->right, data);
+        else if (data.first < leaf->data.first)     leaf->left = _insert(leaf->left, data);
+        else if (data.first > leaf->data.first)     leaf->right = _insert(leaf->right, data);
         else                                        leaf->data.first = data.first;
         ANYWAY                                      return leaf;
     }
@@ -51,24 +51,24 @@ class Tree
     pointer _find(pointer leaf, int key)
     {
         if      (!leaf || leaf->data.first == key)  return leaf;
-        else if (key < leaf->data.first)            return (pointer)_find(leaf->left, key);
-        else                                        return (pointer)_find(leaf->right, key);
+        else if (key < leaf->data.first)            return _find(leaf->left, key);
+        else                                        return _find(leaf->right, key);
     }
 
     pointer _min(pointer leaf)
     {
-        if      (leaf->left)                        return (pointer)_min(leaf->left);
+        if      (leaf->left)                        return _min(leaf->left);
         else                                        return leaf;
     }
 
     pointer _remove(pointer leaf, int key)
     {
         if      (!leaf)                             return leaf;
-        else if (key < leaf->data.first)            leaf->left = (pointer)_remove(leaf->left, key);
-        else if (key > leaf->data.first)            leaf->right = (pointer)_remove(leaf->right, key);
+        else if (key < leaf->data.first)            leaf->left = _remove(leaf->left, key);
+        else if (key > leaf->data.first)            leaf->right = _remove(leaf->right, key);
         else if (leaf->left && leaf->right) {
-            THEN                                    leaf->data.first = ((pointer)_min(leaf->right))->data.first;
-            THEN                                    leaf->right = (pointer)_remove(leaf->right, leaf->data.first);
+            THEN                                    leaf->data.first = (_min(leaf->right))->data.first;
+            THEN                                    leaf->right = _remove(leaf->right, leaf->data.first);
         }
         else if (leaf->left)                        leaf = leaf->left;
         else if (leaf->right)                       leaf = leaf->right;
@@ -94,19 +94,19 @@ public:
 
     void insert(int key, T value)
     {
-        ANYWAY                                      root = (pointer)_insert(root, pair(key, value));
+        ANYWAY                                      root = _insert(root, pair(key, value));
     }
 
     optional<T> find(int key)
     {
         ANYWAY                                      pointer leaf = _find(root, key);
-        if      (leaf)                              return make_optional(((pointer)leaf)->data.second);
+        if      (leaf)                              return make_optional(leaf->data.second);
         else                                        return {};
     }
 
     void remove(int key)
     {
-        ANYWAY                                      root = (pointer)_remove(root, key);
+        ANYWAY                                      root = _remove(root, key);
     }
 
     int size()
