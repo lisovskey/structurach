31df02612c5ca0f7111a56851ddd98d41bdd42a6
lisovskey <ritofzeed@gmail.com>
2017-04-12 20:04:03 +0300

    iterators

--- include/sort.h
+++ include/sort.h
@@ -7,32 +7,33 @@
 #define SORT_H
 
 #include <vector>
-#include <functional>
-#include <iostream>
+#include <iterator>
 
-using std::vector;
+using std::prev;
+using std::next;
 using std::swap;
-using std::function;
 
-template <typename T>
-void bsort(vector<T> &data, function<bool(T, T)> compare) {
+template <typename It, typename Function>
+void bsort(It first, It last, Function compare)
+{
     bool swapped;
     do {
         swapped = false;
-        for (size_t i = 1; i < data.size(); i++) {
-            if (compare(data[i], data[i - 1])) {
-                swap(data[i], data[i - 1]);
+        for (auto i = next(first); i != last; i++) {
+            if (compare(*i, *(prev(i)))) {
+                swap(*i, *prev(i));
                 swapped = true;
             }
         }
     } while (swapped);
 }
 
-template <typename T>
-void isort(vector<T> &data, function<bool(T, T)> compare) {
-    for (size_t i = 0; i < data.size(); i++) {
-        for (size_t j = i; j > 0 && compare(data[j], data[j - 1]); j--) {
-            swap(data[j], data[j - 1]);
+template <typename It, typename Function>
+void isort(It first, It last, Function compare)
+{
+	for (auto i = first; i != last; i++) {
+        for (auto j = i; j != first && compare(*j, *prev(j)); j--) {
+            swap(*j, *prev(j));
         }
     }
 }
