cf642e24e03a7d96aa5313eb28fcaa60c5772c5a
lisovskey <ritofzeed@gmail.com>
2017-04-21 17:30:10 +0300

    shell, cleanup

--- include/sort.h
+++ include/sort.h
@@ -6,36 +6,46 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <vector>
-#include <iterator>
-
-using std::prev;
-using std::next;
 using std::swap;
 
-template <typename It, typename Function>
-void bsort(It first, It last, Function compare)
+template <typename Iterator, typename Function>
+void bsort(Iterator first, Iterator last, Function compare)
+/// bubble sort
 {
     bool swapped;
     do {
         swapped = false;
-        for (auto i = next(first); i != last; i++) {
-            if (compare(*i, *prev(i))) {
-                swap(*i, *prev(i));
+        for (Iterator i = first + 1; i != last; ++i) {
+            if (compare(*i, *(i - 1))) {
+                swap(*i, *(i - 1));
                 swapped = true;
             }
         }
     } while (swapped);
 }
 
-template <typename It, typename Function>
-void isort(It first, It last, Function compare)
+template <typename Iterator, typename Function>
+void isort(Iterator first, Iterator last, Function compare)
+/// insertion sort
 {
-    for (auto i = next(first); i != last; i++) {
-        for (auto j = i; j != first && compare(*j, *prev(j)); j--) {
-            swap(*j, *prev(j));
+	for (Iterator i = first + 1; i != last; ++i) {
+		for (Iterator j = i; j != first && compare(*j, *(j - 1)); --j) {
+            swap(*j, *(j - 1));
         }
     }
+}
+
+template <typename Iterator, typename Function>
+void shsort(Iterator first, Iterator last, Function compare)
+/// shell sort
+{
+	for (auto d = (last - first) / 2; d > 0; d /= 2) {
+		for (Iterator i = first + d; i != last; ++i) {
+			for (Iterator j = i; j - first >= d && compare(*j, *(j - d)); j -= d) {
+				swap(*j, *(j - d));
+			}
+		}
+	}
 }
 
 #endif
