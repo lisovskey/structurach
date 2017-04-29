816afe573ca048dcd1eabaee3217bc549ed364b7
lisovskey <ritofzeed@gmail.com>
2017-04-29 18:29:25 +0300

    ssort, iterators specification

--- include/sort.h
+++ include/sort.h
@@ -6,41 +6,65 @@
 #ifndef SORT_H
 #define SORT_H
 
-template <typename Iterator, typename Function>
-void bsort(Iterator first, Iterator last, Function compare)
+#include <utility>
+#include <iterator>
+
+using std::prev;
+using std::next;
+using std::swap;
+
+template <typename BidIt, typename Pred>
+void bsort(BidIt first, BidIt last, Pred compare)
 /// bubble sort
 {
 	bool swapped;
 	do {
 		swapped = false;
-		for (Iterator i = first + 1; i != last; ++i) {
-			if (compare(*i, *(i - 1))) {
-				std::swap(*i, *(i - 1));
+		for (BidIt i = next(first); i != last; ++i) {
+			if (compare(*i, *prev(i))) {
+				swap(*i, *prev(i));
 				swapped = true;
 			}
 		}
 	} while (swapped);
 }
 
-template <typename Iterator, typename Function>
-void isort(Iterator first, Iterator last, Function compare)
+template <typename BidIt, typename Pred>
+void isort(BidIt first, BidIt last, Pred compare)
 /// insertion sort
 {
-	for (Iterator i = first + 1; i != last; ++i) {
-		for (Iterator j = i; j != first && compare(*j, *(j - 1)); --j) {
-			std::swap(*j, *(j - 1));
+	for (BidIt i = next(first); i != last; ++i) {
+		for (BidIt j = i; j != first && compare(*j, *prev(j)); --j) {
+			swap(*j, *prev(j));
 		}
 	}
 }
 
-template <typename Iterator, typename Function>
-void shsort(Iterator first, Iterator last, Function compare)
+template <typename BidIt, typename Pred>
+void ssort(BidIt first, BidIt last, Pred compare)
+/// selection sort
+{
+	for (BidIt i = first; i != prev(last); ++i) {
+		BidIt tmp = i;
+		for (BidIt j = next(i); j != last; ++j) {
+			if (compare(*j, *tmp)) {
+				tmp = j;
+			}
+		}
+		if (tmp != i) {
+			swap(*i, *tmp);
+		}
+	}
+}
+
+template <typename RanIt, typename Pred>
+void shsort(RanIt first, RanIt last, Pred compare)
 /// shell sort
 {
 	for (auto d = (last - first) / 2; d > 0; d /= 2) {
-		for (Iterator i = first + d; i != last; ++i) {
-			for (Iterator j = i; j - first >= d && compare(*j, *(j - d)); j -= d) {
-				std::swap(*j, *(j - d));
+		for (RanIt i = first + d; i != last; ++i) {
+			for (RanIt j = i; j - first >= d && compare(*j, *(j - d)); j -= d) {
+				swap(*j, *(j - d));
 			}
 		}
 	}
